a3dc8c84b4dedcd13fe1af08adbf48983e6bb02d
kk-open <2441765916@qq.com>
2025-08-21 15:27:03 +0800

    可以用的版本，磁力计待校准

--- main/main.c
+++ main/main.c
@@ -40,14 +40,23 @@ static const char *TAG = "main";
 
 #define I2C_MASTER_NUM I2C_NUM_0 /*!< I2C port number for master dev */
 
-calibration_t cal = {
-    .mag_offset = {.x = 25.183594, .y = 57.519531, .z = -62.648438},
-    .mag_scale = {.x = 1.513449, .y = 1.557811, .z = 1.434039},
-    .accel_offset = {.x = 0.020900, .y = 0.014688, .z = -0.002580},
-    .accel_scale_lo = {.x = -0.992052, .y = -0.990010, .z = -1.011147},
-    .accel_scale_hi = {.x = 1.013558, .y = 1.011903, .z = 1.019645},
+// calibration_t cal = {
+//     .mag_offset = {.x = 25.183594, .y = 57.519531, .z = -62.648438},
+//     .mag_scale = {.x = 1.513449, .y = 1.557811, .z = 1.434039},
+//     .accel_offset = {.x = 0.020900, .y = 0.014688, .z = -0.002580},
+//     .accel_scale_lo = {.x = -0.992052, .y = -0.990010, .z = -1.011147},
+//     .accel_scale_hi = {.x = 1.013558, .y = 1.011903, .z = 1.019645},
 
-    .gyro_bias_offset = {.x = 0.303956, .y = -1.049768, .z = -0.403782}};
+//     .gyro_bias_offset = {.x = 0.303956, .y = -1.049768, .z = -0.403782}};
+
+calibration_t cal = {
+    .mag_offset = {.x = 94.429688, .y = 94.445312, .z = -175.171875},
+    .mag_scale = {.x = 1.166693, .y = 0.979872, .z = 0.890999},
+    .accel_offset = {.x = -0.010246, .y = 0.029886, .z = -0.048280},
+    .accel_scale_lo = {.x = 0.997959, .y = 1.013805, .z = 0.999614},
+    .accel_scale_hi = {.x = -1.004970, .y = -0.985615, .z = -1.014632},
+
+    .gyro_bias_offset = {.x = -0.024856, .y = -0.631924, .z = -0.333009}};
 
 /**
  * Transformation:
